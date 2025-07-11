046f06eb82bfb0ae89bec89038f792436344e0c1
Jepoy <poyhidalgo@gmail.com>
2025-07-11 12:23:05 +0800

    chore: add dark mode and navbar for the pages.

---
+++ src/aica_frontend/components/Hero.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import React from 'react'
+import { HeroParallax } from './ui/hero-parallax'
+
+export default function Hero() {
+  return (
+    <>
+      <HeroParallax products={[]} />
+    </>
+  )
+}
---
+++ src/aica_frontend/components/Navbar.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import {
+  Navbar as ResizableNavbar,
+  NavBody,
+  NavItems,
+  MobileNav,
+  NavbarLogo,
+  NavbarButton,
+  MobileNavHeader,
+  MobileNavToggle,
+  MobileNavMenu,
+} from "@/components/ui/resizable-navbar";
+import { useState } from "react";
+import { ModeToggle } from "./ModeToggle";
+ 
+export function Navbar() {
+  const navItems = [
+    {
+      name: "Home",
+      link: "/",
+    },
+    {
+      name: "About",
+      link: "/about",
+    },
+    {
+      name: "Paper",
+      link: "/paper",
+    },
+  ];
+ 
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+ 
+  return (
+    <div className="relative w-full">
+      <ResizableNavbar>
+        {/* Desktop Navigation */}
+        <NavBody>
+          <NavbarLogo />
+          <NavItems items={navItems} />
+          <div className="flex items-center gap-4">
+            <ModeToggle />
+            <NavbarButton variant="secondary">Login</NavbarButton>
+            <NavbarButton variant="primary">Signup</NavbarButton>
+          </div>
+        </NavBody>
+ 
+        {/* Mobile Navigation */}
+        <MobileNav>
+          <MobileNavHeader>
+            <NavbarLogo />
+            <MobileNavToggle
+              isOpen={isMobileMenuOpen}
+              onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
+            />
+          </MobileNavHeader>
+ 
+          <MobileNavMenu
+            isOpen={isMobileMenuOpen}
+            onClose={() => setIsMobileMenuOpen(false)}
+          >
+            <ModeToggle />
+            {navItems.map((item, idx) => (
+              <a
+                key={`mobile-link-${idx}`}
+                href={item.link}
+                onClick={() => setIsMobileMenuOpen(false)}
+                className="relative text-neutral-600 dark:text-neutral-300"
+              >
+                <span className="block">{item.name}</span>
+              </a>
+            ))}
+            <div className="flex w-full flex-col gap-4">
+              <NavbarButton
+                onClick={() => setIsMobileMenuOpen(false)}
+                variant="primary"
+                className="w-full"
+              >
+                Login
+              </NavbarButton>
+              <NavbarButton
+                onClick={() => setIsMobileMenuOpen(false)}
+                variant="primary"
+                className="w-full"
+              >
+                Register
+              </NavbarButton>
+            </div>
+          </MobileNavMenu>
+        </MobileNav>
+      </ResizableNavbar>
+    </div>
+  );
+}
